d3715ec370a7bdc42de0ff441dd3a3d6f1f6f97c
Azulax-Monk <mrazul17052002@gmail.com>
2021-10-22 20:23:34 +0300

    Initial commit

---
+++ ViewModels/AffineTransformationsViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_TestProject2.ViewModels
+{
+    class AffineTransformationsViewModel : ViewModelBase
+    {
+    }
+}
---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_TestProject2.ViewModels
+{
+    class MainViewModel : ViewModelBase
+    {
+        public ViewModelBase CurrentViewModel { get;  }
+
+        public MainViewModel()
+        {
+            CurrentViewModel = new MenuViewModel();
+        }
+    } 
+}
