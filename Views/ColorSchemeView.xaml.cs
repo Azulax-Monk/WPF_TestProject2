c8050246557cdddfa642a0b23d00ff5d13902da4
Katerino4ka <imkofu@gmail.com>
2021-11-08 20:34:22 +0200

    Drag selection

--- Views/ColorSchemeView.xaml.cs
+++ Views/ColorSchemeView.xaml.cs
@@ -1,6 +1,10 @@
 ﻿
+using System;
+using System.Windows;
 using System.Windows.Controls;
-
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using WPF_TestProject2.ViewModels;
 
 namespace WPF_TestProject2.Views
 {
@@ -12,6 +16,33 @@ namespace WPF_TestProject2.Views
         public ColorSchemeView()
         {
             InitializeComponent();
+
+        }
+
+        private void Canvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var mousePosition = e.GetPosition(sender as UIElement);
+            Canvas.SetLeft(selectionRectangle, mousePosition.X);
+            Canvas.SetTop(selectionRectangle, mousePosition.Y);
+            selectionRectangle.Visibility = System.Windows.Visibility.Visible;
+
+        }
+
+        private void Canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                var mousePosition = e.GetPosition(sender as UIElement);
+                selectionRectangle.Width = Math.Abs( mousePosition.X - Canvas.GetLeft(selectionRectangle));
+                selectionRectangle.Height = Math.Abs(mousePosition.Y - Canvas.GetTop(selectionRectangle));
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ColorSchemeViewModel vm = (ColorSchemeViewModel)this.DataContext;
+            vm.CanvasHeight = canvas.ActualHeight;
+            vm.CanvasWidth = canvas.ActualWidth;
         }
     }
 }
